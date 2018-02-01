2a973a9d3a49b658c9d5a916614f6e23f20208c8
刘赋骁 <32979268+liufuxiao@users.noreply.github.com>
2018-02-02 00:49:02 +0800

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,45 @@
+//This is the upload file. You can select google or Microsoft as the destination
+//to upload the data file.The "add-utterances1.js" and "add-utterances2.js" are 
+//the execution file of Microsoft and google relatively。
+
+var readline = require('readline');
+var exec = require('child_process').exec;
+var Microsoftfilename = 'add-utterances1.js';
+var googlefilename = 'add-utterance2.js';
+var filename ="";
+
+var  rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+
+rl.question("Please enter where you want to upload:",function(answer){
+    if(answer=="Microsoft")
+    {
+        filename = Microsoftfilename;
+    }
+    else {
+        filename = googlefilename;
+    }
+    console.log(filename);
+
+
+   
+exec('node'+' '+filename,function(err,stdout,stderr) {
+    if (err) {
+        console.log('stderr', err);
+    }
+    if (stdout) {
+        console.log(stdout);
+    }
+    });
+
+  // rl.close();
+});
+
+
+// close事件监听
+rl.on("close", function(){
+    process.exit(0);
+});

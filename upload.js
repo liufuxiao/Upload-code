c392f534d3329fb46dc0b353fb3d296c3b46291c
刘赋骁 <32979268+liufuxiao@users.noreply.github.com>
2018-02-06 00:28:50 +0800

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,39 +1,20 @@
 //This is the upload file. You can select google or Microsoft as the destination
-//to upload the data file.The "add-utterances1.js" and "add-utterances2.js" are 
-//the execution file of Microsoft and google relatively。
+//to upload the data file.
 
 var express = require('express');
+var Google = require('./resource');
+var Microsoft = require('./add-utterances');
 var app = express();
-var fs = require("fs");
-var exec = require('child_process').exec;
-var filename1 ="add-utterances1.js";
-var filename2 ="add-utterances2.js";
 
-
-app.get('/addMicrosoft', function (req, res) {
-    exec('node'+' '+filename1,function(err,stdout,stderr) {
-        if (err) {
-            console.log('stderr', err);
-            res.end('stderr', err);
-        }
-        if (stdout) {
-            console.log(stdout);
-            res.end(stdout);
-        }
-    });
-})
 
 app.get('/addGoogle', function (req, res) {
-    exec('node'+' '+filename2,function(err,stdout,stderr) {
-        if (err) {
-            console.log('stderr', err);
-             res.end('stderr', err);
-        }
-        if (stdout) {
-            console.log(stdout);
-            res.end(stdout);
-        }
-    });
+    Google.createIntents("roomrservation");
+    res.end("successful!");
+})
+
+app.get('/addMicrosoft', function (req, res) {
+    Microsoft.say();
+    res.end("successful!");
 })
 
 var server = app.listen(8081, function () {

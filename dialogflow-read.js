c9399bc6ab217c39ccf4df28cf6def750b1cfe70
刘赋骁 <32979268+liufuxiao@users.noreply.github.com>
2018-03-06 17:24:35 +0800

    Create dialogflow-read.js

---
+++ dialogflow-read.js
@@ -0,0 +1,40 @@
+'use strict';
+function Read() {
+    this.Read=function(intentId) {
+        const dialogflow = require('dialogflow');
+        const intentsClient = new dialogflow.IntentsClient();
+        const request = {
+            intentView: 'INTENT_VIEW_FULL',
+            name: intentId
+        };
+        intentsClient
+            .getIntent(request)
+            .then(responses => {
+               console.log(responses[0]);
+            var utterance = {
+                "displayName" : "TurnOn",
+                "trainingPhrases":[]
+            }
+            responses[0].trainingPhrases.map(function(item,index){
+                utterance.trainingPhrases.push({
+                    "type": "TYPE_EXAMPLE",
+                    "parts": []
+                });
+                item.parts.map(function(item1,index1){
+                    if(item1.entityType){
+                        utterance.trainingPhrases[index].parts.push({"text": item1.text, "entityType": item1.entityType});
+                    }else{
+                        utterance.trainingPhrases[index].parts.push({"text": item1.text});
+                    }
+                });
+            });
+            utterance.trainingPhrases.map(function(item,index){
+                console.log(item.parts);
+            });
+    })
+    .catch(err => {
+            console.error(`Failed to get intent ${intent.displayName}`, err);
+    });
+};
+}
+module.exports = Read;

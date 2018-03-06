fb6fb3c8731ce8dc19b54bc9a40698ccad03b5f2
刘赋骁 <32979268+liufuxiao@users.noreply.github.com>
2018-03-07 01:32:22 +0800

    Update luis-update.js

--- luis-update.js
+++ luis-update.js
@@ -1,11 +1,11 @@
 function update() {
-    var De = require('./luis-delete');
-    var Microsoft = require('./luis-add');
-    di = new De();
-    Mi = new Microsoft();
     this.update = function (myObj2,exampleId) {
-        di.deleteUtt(exampleId);
-        Mi.uploadPhase(myObj2);
+        var D = require('./luis-delete');
+        var Mi = require('./luis-add');
+        d = new D();
+        M = new Mi();
+        d.deleteUtt(exampleId);
+        M.uploadPhase(myObj2);
     };
 }
 module.exports=update;

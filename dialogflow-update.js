e5f93c574864e2a0d5d035c43adaec08d73a6f29
刘赋骁 <32979268+liufuxiao@users.noreply.github.com>
2018-03-06 17:28:48 +0800

    Create dialogflow-update.js

---
+++ dialogflow-update.js
@@ -0,0 +1,31 @@
+function updateIntent(){
+    const grpc = require('grpc');
+    const prompt = require('prompt');
+    this.updateIntent=function(intentname,Intent,projectId) {
+        const dialogflow = require('dialogflow');
+        const intentsClient = new dialogflow.IntentsClient();
+        const agentPath = intentsClient.projectAgentPath(projectId);
+        const request = {name: intentname};
+        intentsClient
+            .deleteIntent(request)
+            .then(() => {
+            console.log(`Intent`);
+    })
+    .catch(err => {
+            console.error(`Failed to delete intent:`, err);
+    });
+        const Request1 = {
+            parent: agentPath,
+            intent: Intent,
+        };
+        intentsClient
+            .createIntent(Request1)
+            .then(responses => {
+            console.log("update:");
+    })
+    .catch(err => {
+            console.error('ERROR:', err);
+    });
+    }
+}
+module.exports = updateIntent;

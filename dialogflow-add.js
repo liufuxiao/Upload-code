2fc28f81f1fcb057486d4dbfcae41cf535735241
刘赋骁 <32979268+liufuxiao@users.noreply.github.com>
2018-03-06 17:21:30 +0800

    Update and rename add-Google.js to dialogflow-add.js

--- dialogflow-add.js
+++ dialogflow-add.js
@@ -1,16 +1,7 @@
-//This is the upload file of google.
-//It first read the data file and transfer it to the upload file.
-//Because of lacking the permission to certain module of nodejs,
-//invoke the python file to finish it.
-
-
 'use strict';
 function createIntents() {
-    const grpc = require('grpc');
-    const prompt = require('prompt');
     this.uploadPhase =function(Intent,projectId){
        const dialogflow = require('dialogflow');
-       const contextsClient = new dialogflow.ContextsClient();
        const intentsClient = new dialogflow.IntentsClient();
        const agentPath = intentsClient.projectAgentPath(projectId);
     const Request = {
@@ -21,12 +12,9 @@ function createIntents() {
         .createIntent(Request)
         .then(responses => {
         console.log("Intent has been created:");
-    //console.log(JSON.stringify(responses));
 })
 .catch(err => {
         console.error('ERROR:', err);
 });
     };
 }
-
-module.exports = createIntents;

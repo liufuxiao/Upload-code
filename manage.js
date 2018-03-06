9c534e7b528bf364e1789e6f9a9df95a49933fe8
刘赋骁 <32979268+liufuxiao@users.noreply.github.com>
2018-03-07 01:26:20 +0800

    Update manage.js

--- manage.js
+++ manage.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var app = express();
 var Microsoft = require('./luis-add');
@@ -5,17 +6,17 @@ var Re =require('./luis-read');
 var De =require('./luis-delete');
 var Up2 =require('./luis-update');
 var Google = require('./dialogflow-add');
-var Di =require('./dialogflow-read');
+var Ea =require('./dialogflow-read');
 var Dd =require('./dialogflow-delete');
 var Up1 =require('./dialogflow-update');
 Mi = new Microsoft();
-Go = new Google();
 re =new Re();
 de =new De();
-di =new Di();
+up2 =new Up2();
+Go = new Google();
+di =new Ea();
 dd =new Dd();
 up =new Up1();
-up2 =new Up2();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static('public'));
@@ -35,7 +36,7 @@ app.post('/add', urlencodedParser, function (req, res) {
 
 app.post('/read', urlencodedParser, function (req, res) {
     if(req.body.destination==="Google"){
-        //var intentId ='projects/roomrservation/agent/intents/35598eac-c8c7-4547-8fd2-2fc75b12ecfb';
+        //var intentId ='projects/roomrservation/agent/intents/791f16e2-d0d5-4012-9d9c-34307cf07cfe';
         di.Read(req.body.intentId);
         res.end("successful!");
     }else if(req.body.destination==="Microsoft") {
@@ -49,7 +50,7 @@ app.post('/read', urlencodedParser, function (req, res) {
 
 app.post('/delete', urlencodedParser, function (req, res) {
     if(req.body.destination==="Google") {
-        //var intentId ='projects/roomrservation/agent/intents/35598eac-c8c7-4547-8fd2-2fc75b12ecfb';
+        //var intentId ='projects/roomrservation/agent/intents/791f16e2-d0d5-4012-9d9c-34307cf07cfe';
         dd.deleteIntent(req.body.intentId);
         res.end("successful!");
     }else if(req.body.destination==="Microsoft") {
@@ -59,28 +60,4 @@ app.post('/delete', urlencodedParser, function (req, res) {
     }else{
         res.end("Please enter the correct destination")
     }
-});
-
-app.post('/update', urlencodedParser, function (req, res) {
-    if(req.body.destination==="Google") {
-        //var intentId ='projects/roomrservation/agent/intents/35598eac-c8c7-4547-8fd2-2fc75b12ecfb';
-        //var projectId = 'roomrservation'
-        up.updateIntent(req.body.intentId,JSON.parse(req.body.utterance),req.body.projectId);
-        res.end("successful!");
-    }else if(req.body.destination==="Microsoft") {
-        //var exampleId="-69067"
-        up2.update(JSON.parse(req.body.utterance),req.body.exampleId);
-        res.end("successful!");
-    }else{
-        res.end("Please enter the correct destination")
-    }
-});
-
-var server = app.listen(8081, function () {
-
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
 });

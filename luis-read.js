098959eb8158bcce49cb84e4e34d546b99f67a26
刘赋骁 <32979268+liufuxiao@users.noreply.github.com>
2018-03-10 00:28:43 +0800

    Update luis-read.js

--- luis-read.js
+++ luis-read.js
@@ -1,21 +1,14 @@
-function read(){
-    var rp = require('request-promise');
-    const LUIS_programmaticKey = "67fa05fd36ca4d4cbc8c2eb91e41dc10";
-    const LUIS_appId = "6a18e3d5-7267-4b94-b1c4-8944b866fbc8";
-    const LUIS_versionId = "0.1";
-    this.Read =function(intent){
-        var utterance ={
-            "displayName": intent,
-            "trainingPhrases": []
+const rp = require('request-promise');
+const LUIS =require('./LUIS_account');
+const Read =() =>{
+        const configAddUtterance = {
+            LUIS_subscriptionKey: LUIS.programmaticKey,
+            LUIS_appId: LUIS.appId,
+            LUIS_versionId: LUIS.versionId,
+            uri: `https://westus.api.cognitive.microsoft.com/luis/api/v2.0/apps/${LUIS.appId}/versions/${LUIS.versionId}/examples`
         };
-        var configAddUtterance = {
-            LUIS_subscriptionKey: LUIS_programmaticKey,
-            LUIS_appId: LUIS_appId,
-            LUIS_versionId: LUIS_versionId,
-            uri: "https://westus.api.cognitive.microsoft.com/luis/api/v2.0/apps/{appId}/versions/{versionId}/examples".replace("{appId}", LUIS_appId).replace("{versionId}", LUIS_versionId)
-        };
-        var readUtterance = async (config) => {
-            var utterancePromise = sendUtteranceToApi({
+        const readUtterance = async (config) => {
+            return sendUtteranceToApi({
                 uri: config.uri,
                 method: 'GET',
                 headers: {
@@ -24,53 +17,54 @@ function read(){
                 json: true,
                 body: {}
             });
-            let results = await utterancePromise;
-        }
-        var sendUtteranceToApi = async (options) => {
+        };
+        const sendUtteranceToApi = async (options) => {
             try {
-                    var response = await rp.get(options);
-                    var a = response.filter(function(item){
-                    return item.intentLabel === intent;
-                });
-                    //The following console.log is to get the id.
-                    console.log(a);
-                    a.map(function(item,index){
-                        utterance.trainingPhrases.push({
-                            "type":"TYPE_EXAMPLE",
-                            "parts":[]
-                    });
-                    var c=[];
-                    item.entityLabels.map(function(item1,index1){
-                        c.push(item1.startTokenIndex);
-                        c.push(item1.endTokenIndex+1);
-                    });
-                    c.push(item.tokenizedText.length);
-                    c.reduce(function(prev,next){
-                        var flag = 0;
-                        item.entityLabels.map(function(item2,index2){
-                            if(prev!==next && prev===item2.startTokenIndex && next===item2.endTokenIndex+1){
-                                flag =1;
-                                utterance.trainingPhrases[index].parts.push({text: item.tokenizedText.slice(prev, next).join(" ")+" ",entityType: "@"+item2.entityName});
-                            }
-                        });
-                        if(prev!==next && flag===0){
-                            utterance.trainingPhrases[index].parts.push({text: item.tokenizedText.slice(prev, next).join(" ")+" "});
-                        }
-                        return next;
-                    },0);
-                });
-
-                    console.log(utterance);
-                    utterance.trainingPhrases.map(function(item3,index3){
-                    console.log(item3.parts);
-
-                });
-                return { request: options.body, response: response };
+                    return await rp.get(options);
             } catch (err) {
                 throw err;
             }
-        }
-        readUtterance(configAddUtterance);
         };
-}
-module.exports = read;
+        return readUtterance(configAddUtterance);
+        };
+
+const transform =(response,intent) => {
+    const utterance ={
+        "displayName": intent,
+        "trainingPhrases": []
+    };
+    const a = response.filter(item =>{
+        return item.intentLabel === intent;
+    });
+    a.map((item,index)=>{
+        utterance.trainingPhrases.push({
+            "type":"TYPE_EXAMPLE",
+            "parts":[]
+        });
+        let c=[];
+        item.entityLabels.map(item1 =>{
+            c.push(item1.startTokenIndex,item1.endTokenIndex+1);
+        });
+        c.push(item.tokenizedText.length);
+        c.reduce((prev,next) =>{
+            let flag = 0;
+            item.entityLabels.map( item2 =>{
+                if(prev!==next && prev===item2.startTokenIndex && next===item2.endTokenIndex+1){
+                    flag =1;
+                    utterance.trainingPhrases[index].parts.push({text: item.tokenizedText.slice(prev, next).join(" ")+" ",entityType: "@"+item2.entityName});
+                }
+            });
+            if(prev!==next && flag===0){
+                utterance.trainingPhrases[index].parts.push({text: item.tokenizedText.slice(prev, next).join(" ")+" "});
+            }
+            return next;
+        },0);
+    });
+    return utterance;
+};
+
+module.exports={
+    read: Read,
+    tran:transform
+};
+

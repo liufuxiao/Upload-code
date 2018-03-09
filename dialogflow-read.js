eb78b96435eb5e4262c25e858e5af0a434d352c9
刘赋骁 <32979268+liufuxiao@users.noreply.github.com>
2018-03-10 00:26:11 +0800

    Update dialogflow-read.js

--- dialogflow-read.js
+++ dialogflow-read.js
@@ -1,41 +1,30 @@
-'use strict';
-function Read() {
-    this.Read=function(intentId) {
-        const dialogflow = require('dialogflow');
-        const intentsClient = new dialogflow.IntentsClient();
-        const request = {
-            intentView: 'INTENT_VIEW_FULL',
-            name: intentId
+const dialogflow = require('dialogflow');
+const Read=(intentId) => {
+    const intentsClient = new dialogflow.IntentsClient();
+    const request = {
+        intentView: 'INTENT_VIEW_FULL',
+        name: intentId
         };
-        intentsClient
-            .getIntent(request)
-            .then(responses => {
-            console.log(responses[0]);
-        var utterance = {
-            "displayName" : "TurnOn",
-            "trainingPhrases":[]
-        }
-        responses[0].trainingPhrases.map(function(item,index){
-            utterance.trainingPhrases.push({
-                "type": "TYPE_EXAMPLE",
-                "parts": []
-            });
-            item.parts.map(function(item1,index1){
-                if(item1.entityType){
-                    utterance.trainingPhrases[index].parts.push({"text": item1.text, "entityType": item1.entityType});
-                }else{
-                    utterance.trainingPhrases[index].parts.push({"text": item1.text});
-                }
-            });
-        });
-        utterance.trainingPhrases.map(function(item,index){
-            console.log(item.parts);
-        });
-    })
-    .catch(err => {
-            console.error(`Failed to get intent`, err);
-    });
+    return intentsClient.getIntent(request);
     };
-}
-module.exports = Read;
+
+const transform =(responses) => {
+    let utterance = {
+        "displayName" : "TurnOn",
+        "trainingPhrases":[]
+    };
+    utterance.trainingPhrases = responses[0].trainingPhrases.map(item => ({
+            type: "TYPE_EXAMPLE",
+            parts: item.parts.map(item1 => Object.assign({ text: item1.text },
+                item1.entityType ? { entityType: item1.entityType } : {} )
+            )
+        })
+    );
+    return utterance;
+};
+
+module.exports={
+    read: Read,
+    tran:transform
+};
 
